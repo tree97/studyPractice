412ba89098a37120fe5703b4748bf02da2466e1d
V_Matskevich <matskevich1997@gmail.com>
2016-02-12 22:05:14 +0200

    laba1.2 v2 reading .json doesn't work

--- laba1.2/src/mainpackage/Chat.java
+++ laba1.2/src/mainpackage/Chat.java
@@ -1,5 +1,6 @@
 package mainpackage;
-
+import com.google.gson.*;
+import java.io.*;
 /**
  I hope, that adding messages will be in chronological order, if no, i will add sorting
  */
@@ -8,8 +9,8 @@ public class Chat {
     private static int containerSize=10;
     private static int lastId=0;
     private static Message history[]=new Message[containerSize];
-    private static String copyArgs[];    ///  temp, will be removed later with adding the .json files
-    private static void addMessage(int i){          /// temp, will be seriously edited later with adding the .json files support
+    private static String copyArgs[];    ///  temp, may be will be removed later
+    private static void sizeController(){
         Message buf[];
         if(messageCount==containerSize){
             buf=new Message[containerSize];
@@ -20,6 +21,9 @@ public class Chat {
                 history[j]=new Message(buf[j]);
             containerSize*=2;
         }
+    }
+    private static void addMessage(int i){
+        sizeController();
         String text=new String(copyArgs[i+1]);
         String author=new String(copyArgs[i+2]);
         String time=new String(copyArgs[i+3]);
@@ -41,16 +45,38 @@ public class Chat {
             messageCount--;
         }
     }
-    private static void showHistory(int i){     /// temp, will be edited later with small changes
+    private static void showHistory(){     /// temp, will be edited later with small changes
         for(int j=0;j<messageCount;j++){
             System.out.println(history[j].toString()+"\n");
         }
     }
-    public static void main(String args[]) {
+    private static void downloadFromFile(String fileName) throws IOException{     /// temp, will be modified after changing ID
+        messageCount=0;
+        Reader reader=new InputStreamReader(Chat.class.getResourceAsStream(fileName),"UTF-8");
+        Gson gson=new GsonBuilder().create();
+        Integer temp =new Integer(gson.fromJson(reader,Integer.class));
+        int count=temp.intValue();
+        for(messageCount=0;messageCount<count;messageCount++){
+            sizeController();
+            history[messageCount]=new Message(gson.fromJson(reader,Message.class));
+            lastId=history[messageCount].getId();
+        }
+        reader.close();
+    }
+    private static void saveHistory()throws IOException{
+        Writer saving=new OutputStreamWriter(new FileOutputStream("output.json"),"UTF-8");
+        Gson gson=new GsonBuilder().create();
+        gson.toJson(new Integer(messageCount),saving);
+        for(int i=0;i<messageCount;i++) {
+            gson.toJson(history[i],saving);
+        }
+        saving.close();
+    }
+    public static void main(String args[]) throws IOException{
 
         ///    temp, will be removed later with adding the .json files
-
-        copyArgs = new String[args.length];
+        downloadFromFile("output.json");
+     /*   copyArgs = new String[args.length];
         int i=0;
         for(i=0;i<args.length;i++)
             copyArgs[i]=new String(args[i]);
@@ -65,11 +91,12 @@ public class Chat {
                 i+=2;
             }
             else if("3".equals(args[i])){
-                showHistory(i);
+                showHistory();
                 i++;
             }
         }
-
+        saveHistory(); */
+        showHistory();
         ///
     }
 }
