bca1845b953ffbaa8f1c8b25f19faedb137c7aa4
tree97 <matskevich1997@gmail.com>
2016-05-10 22:37:58 +0300

    lab12 rc2

---
+++ WebAppChat2/src/main/java/by.bsu.fpmi.up/ChatServlet.java
@@ -0,0 +1,33 @@
+package by.bsu.fpmi.up;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Scanner;
+
+@WebServlet(value = "/homepage.html")
+public class ChatServlet extends HttpServlet {
+
+    private static final String PARAM_USERNAME = "user";
+    private static final String PARAM_PASSWORD = "pass";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
